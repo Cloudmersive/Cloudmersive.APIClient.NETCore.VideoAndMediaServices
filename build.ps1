5dac154909fd2a6bf787ca850de1f8a14cbe1d79
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2021-06-17 21:17:11 -0700

    2.0.5

--- build.ps1
+++ build.ps1
@@ -17,6 +17,8 @@
 (Get-Content '.\client\src\Cloudmersive.APIClient.NETCore.VideoAndMediaServices\Cloudmersive.APIClient.NETCore.VideoAndMediaServices.csproj').replace('<PackageReference Include="RestSharp" Version="105.1.0" />', '<PackageReference Include="RestSharp" Version="106.6.10" />') | Set-Content '.\client\src\Cloudmersive.APIClient.NETCore.VideoAndMediaServices\Cloudmersive.APIClient.NETCore.VideoAndMediaServices.csproj'
 (Get-Content '.\client\src\Cloudmersive.APIClient.NETCore.VideoAndMediaServices\Client\ApiClient.cs').replace('request.AddFile(param.Value.Name, param.Value.Writer, param.Value.FileName, param.Value.ContentType);', 'request.AddFile(param.Value.Name, param.Value.Writer, param.Value.FileName, param.Value.ContentLength, param.Value.ContentType);') | Set-Content '.\client\src\Cloudmersive.APIClient.NETCore.VideoAndMediaServices\Client\ApiClient.cs'
 
+(Get-Content ./client/src/Cloudmersive.APIClient.NETCore.VideoAndMediaServices/Cloudmersive.APIClient.NETCore.VideoAndMediaServices.csproj).replace('</ItemGroup>', '</ItemGroup><Target Name="PostBuild" AfterTargets="PostBuildEvent">    <Exec Command="call powershell C:\CodeSigning\sign.ps1  $(TargetPath)" />  </Target>') | Set-Content ./client/src/Cloudmersive.APIClient.NETCore.VideoAndMediaServices/Cloudmersive.APIClient.NETCore.VideoAndMediaServices.csproj
+
 & dotnet build ./client/src/Cloudmersive.APIClient.NETCore.VideoAndMediaServices/Cloudmersive.APIClient.NETCore.VideoAndMediaServices.csproj -c Release
 & dotnet pack ./client/src/Cloudmersive.APIClient.NETCore.VideoAndMediaServices/Cloudmersive.APIClient.NETCore.VideoAndMediaServices.csproj -c Release
 
